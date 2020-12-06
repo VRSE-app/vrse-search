870dd6a845c35f4546f1dd071474b9cd0f496a4b
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-12-06 14:40:16 +0000

    removed unused project files

--- server/health.js
+++
@@ -1,7 +0,0 @@
-var client = require("./connection.js");
-
-console.log(client);
-/* Get the health status */
-client.cluster.health({}, function (err, resp, status) {
-    console.log("-- Client Health --", resp);
-});
