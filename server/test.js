3b6ea68939d0f59a1d5275debd43dddc88892c89
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-12-09 13:14:20 +0000

    attempt to upload data

--- server/test.js
+++ server/test.js
@@ -4,30 +4,83 @@ const zlib = require('zlib');
 const readline = require('readline');
 require('array.prototype.flatmap');
 const { client, deleteIndex, checkConnection, initMapping, createIndex, resetIndex } = require('./connection');
+const { concat } = require('lodash');
 
 const directoryFiles = fs.readdirSync('./publications');
 
 // If you want to clear everything and re-import run resetIndex()
+// deleteIndex();
+// createIndex();
+// initMapping();
 // resetIndex();
 
-// Concurrently process all files in directory (cannot do )
-Promise.all(directoryFiles.map(filename => {
-    return new Promise(async (resolve, reject) => {
-        if (path.extname(filename) === '.gz') {
-            console.log(filename);
-            let readPath = `./publications/${filename}`
-            let writePath = `./publications/${filename.slice(0, -3)}`
+// runs concurrent promises in n-sized groups of zip files
+function main() {
+    let groupNum = 1;
+    let filenamePrefix = "s2-corpus-";
+    for (let i = 0; i < 10; i += 5) {
+        console.log("ZIP Group: ", groupNum);
+
+        for (let j = 0; j < 5; j++) {
+            let num = i + j;
+            let str = num.toString().padStart(3, "0")
+            let res = filenamePrefix.concat(str);
+            res = res.concat(".gz");
+            let readPath = `./publications/${res}`;
+            let writePath = `./publications/${res.slice(0, -3)}`;
+
+            console.log("readPath:", readPath);
+
             try {
                 await upload_data(readPath, writePath);
-                resolve("done")
+                // resolve("done with group");
             } catch (err) {
                 console.log(err);
                 reject(err);
             }
-
         }
-    })
-}))
+
+        groupNum += 1;
+    }
+}
+
+async function concurrentUploadGroup(groupFileNames) {
+    Promise.all(groupFileNames.map(filename => {
+        new Promise(async (resolve, reject) => {
+            let readPath = `./publications/${filename}`
+            let writePath = `./publications/${filename.slice(0, -3)}`
+
+            try {
+                await upload_data(readPath, writePath);
+                resolve("done with file");
+            } catch (err) {
+                console.log(err);
+                reject(err);
+            }
+        })
+    }))
+}
+
+// Concurrently process all files in directory (cannot do for memory reasons)
+function concurrentUploadAll() {
+    Promise.all(directoryFiles.map(filename => {
+        return new Promise(async (resolve, reject) => {
+            if (path.extname(filename) === '.gz') {
+                console.log(filename);
+                let readPath = `./publications/${filename}`
+                let writePath = `./publications/${filename.slice(0, -3)}`
+                try {
+                    await upload_data(readPath, writePath);
+                    resolve("done")
+                } catch (err) {
+                    console.log(err);
+                    reject(err);
+                }
+
+            }
+        })
+    }))
+}
 
 async function upload_data(directoryPath, writePath) {
     const unzip = zlib.createGunzip();
@@ -51,14 +104,6 @@ async function upload_data(directoryPath, writePath) {
         });
 }
 
-async function insertPublications(chunk) {
-    const body = chunk.flatMap(doc => [{ index: { _index: 'vrse-search', _type: 'publication' } }, doc])
-
-    await client.bulk({ refresh: true, body });
-
-    const res = await client.count({ index: 'vrse-search' })
-    console.log("Count: ", res.count)
-}
 
 async function writeToES(array, writePath) {
     console.log("Array Length:", array.length);
@@ -76,3 +121,20 @@ async function writeToES(array, writePath) {
         console.log("Error:", err);
     });
 }
+
+async function insertPublications(chunk) {
+    const body = chunk.flatMap(doc => [{ index: { _index: 'vrse-search', _type: 'publication' } }, doc])
+
+    await client.bulk({ refresh: true, body });
+
+    const res = await client.count({ index: 'vrse-search' })
+    console.log("Count: ", res.count)
+}
+
+async function countPublications() {
+    const res = await client.count({ index: 'vrse-search' })
+    console.log("Count: ", res.count)
+}
+
+countPublications();
+// main();
