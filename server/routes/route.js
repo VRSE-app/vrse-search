015bb2d563493d340dd3f047c9a8c4b0226dc833
Yannis Panagis <yannis.panagis1998@gmail.com>
2021-03-21 18:53:53 +0000

    finished first step of updated search api

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,16 +1,17 @@
 const { request, response } = require('express');
 const express = require('express');
+const { result } = require('lodash');
 const router = express.Router();
+const { Concurrency } = require('max-concurrency');
 
 const { client } = require("../connection");
-
-// const { Client } = require('@elastic/elasticsearch');
-// const { response } = require('express');
-
 var bodyParser = require("body-parser").json();
 
-// this should probably be refactored and cleaned up into different functions and not sure if the first thing is needed
-// the size here should depend on the number of things they have in the network - do we have to rank these? not clear yet
+// todo: keep original results
+// todo: add incititations as well
+// todo: fix query to only return that one result (done kind of but is this what we want?)
+// todo: remove slice of only the 10 results for both the top and bottom level
+// todo: refactor and make sure it all works concurrently with elastic
 router.get('/_search/:input', (req, res) => {
     client.search({
         index: 'vrse-search',
@@ -23,194 +24,71 @@ router.get('/_search/:input', (req, res) => {
         }
     })
         .then(response => {
-            return res.json(response);
+            const hits = response.body.hits.hits
+            const hitOutCitations = hits
+                .map(hit => hit._source.outCitations)
+                .reduce((acc, cur) => {
+                cur.map(id => {
+                    const presentInArr = acc.findIndex(itemId => id === itemId) > -1
+                    if(!presentInArr) {
+                        acc.push(id)
+                    }
+                })
+                return acc;
+            }, [])
+
+            let outCitationPromises = hitOutCitations.slice(0, 10).map(id => {
+                return new Promise((resolve, reject) => 
+                    client.search({
+                        index: 'vrse-search',
+                        type: 'publication',
+                        body: {
+                            size: 1, // todo: verify this is a good method
+                            query: {
+                                query_string: { query: id }
+                            }
+                        }
+                    }).then(data => {
+                        console.log(`resolved inside: ${data}`)
+                        resolve(data)
+                    })
+                    .catch(err => {
+                        console.log(`rejected inside: ${err}`)
+                        reject(err)
+                    })
+                )
+            })
+            
+            console.log("is this an array of promises?")
+            console.log(outCitationPromises.length)
+            return outCitationPromises
         })
+        .catch(err => {
+            console.log(`rejected outside: ${err}`)
+            reject(err)
+        })
+        .then(requests =>
+            Promise.all(requests.slice(0, 10))
+            .catch(err => console.log(err))
+            .then(responses => {
+                console.log(responses)
+                // anything below here does not run for some reason
+                const extendedResult = [];
+                // const final = body.reduce((acc, cur) => {
+                //     cur.map(result => acc.push(result.body.hits))
+                // }, [])
+                responses.forEach(response => {
+                    extendedResult.push(response.body.hits)
+                })
+            
+                console.log(extendedResult)
+
+                return res.json(extendedResult);
+            })
+        )
         .catch(err => {
             return res.status(500).json({ "message": err })
         })
 })
-
-// router.post('/search', bodyParser, (req, res) => {
-//     client.index({
-//         index: 'vrse-search',
-//         body: req.body
-//     })
-//         .then(resp => {
-//             return res.status(200).json({
-//                 msg: 'publication indexed'
-//             });
-//         })
-//         .catch(err => {
-//             return res.status(500).json({
-//                 msg: 'Error',
-//                 err
-//             });
-//         })
-// });
-
-// router.get('/search/:id', (req, res) => {
-//     let query = {
-//         index: 'vrse-search',
-//         id: req.params.id
-//     }
-
-//     client.index.get(query)
-//         .then(resp => {
-//             if (!resp) {
-//                 return res.status(404).json({
-//                     publication: resp
-//                 });
-//             }
-//             return res.status(200).json({
-//                 publication: resp
-//             });
-//         })
-//         .catch(err => {
-//             return res.status(500).json({
-//                 msg: 'Error not found',
-//                 err
-//             });
-//         });
-// });
-
-// router.put('/search/:id', bodyParser, (req, res) => {
-//     client.update({
-//         index: 'vrse-search',
-//         id: req.params.id,
-//         body: {
-//             doc: req.body
-//         }
-//     })
-//         .then(resp => {
-//             return res.status(200).json({
-//                 msg: 'product updated'
-//             });
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             return res.status(500).json({
-//                 msg: 'Error',
-//                 err
-//             });
-//         })
-// });
-
-// router.delete('/search/:id', (req, res) => {
-//     client.delete({
-//         index: 'vrse-search',
-//         id: req.params.id
-//     })
-//         .then(resp => {
-//             res.status(200).json({
-//                 'msg': 'Product deleted'
-//             });
-//         })
-//         .catch(err => {
-//             res.status(404).json({
-//                 'msg': 'Error'
-//             });
-//         });
-// });
-
-// /_search?q=
-
-
-
-// router.get('/search', (req, res) => {
-//     // syntax is likely not to match with ES
-
-//     // holds the value of the query param searchquery
-//     const searchQuery = req.query.searchQuery;
-
-//     // do somethign if the searchQuery is not null.
-//     if (searchQuery != null) {
-
-//         searchGoogle(searchQuery)
-//             .then(results => {
-//                 //Returns a 200 Status OK with Results JSON back to the client.
-//                 response.status(200);
-//                 response.json(results);
-//             });
-//     } else {
-//         res.end();
-//     }
-
-// searchElastic function to interface with elastic (do we need this or can it be part of the other one)
-// const searchElastic = async (searchQuery) => {
-
-// };
-
-// async function makeSearch() {
-//     const { body } = await client.search({
-//         index: 'vrse-search',
-//         type: 'publication',
-//         body: {
-//             query: {
-//                 match: { title: 'number theory' }
-//             }
-//         }
-//     });
-// }
-// here we are forcing an index refresh, otherwise we will not
-// get any result in the consequent search
-// await client.indices.refresh({ index: 'vrse-search' })
-
-
-// client.search({
-//     index: 'vrse-search',
-//     type: 'publication',
-//     body: {
-//         query: {
-//             match: { title: 'number theory' }
-//         }
-//     }
-// })
-//     .then(resp => {
-//         return res.status(200).json({
-//             publications: resp.hits.hits
-//         });
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         return res.status(500).json({
-//             msg: 'Error',
-//             err
-//         });
-//     });
-
-// By default ES returns 10 results but we can change this by adding size into the query
-// Let's search!
-// const { body } = await client.search({
-//     index: 'vrse-search',
-//     type: 'publication',
-//     body: {
-//         query: {
-//             match: { title: 'number theory' }
-//         }
-//     }
-// });
-// })
-
-// router.get('/search', (req, res) => {
-//     let query = {
-//         index: 'vrse-search',
-//     }
-//     if (req.query.publication) {
-//         query.q = `${req.query.publication}`;
-//     }
-//     client.search(query)
-//         .then(resp => {
-//             return res.status(200).json({
-//                 publications: resp.hits.hits
-//             });
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             return res.status(500).json({
-//                 msg: 'Error',
-//                 err
-//             });
-//         });
-// });
 
 module.exports = router;
